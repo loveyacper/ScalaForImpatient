6256a65ac4c1671b6f23a5b087c263aaef64a05c
loveyacper <bertyoung666@gmail.com>
2015-07-02 13:31:55 +0800

    Create ch1.scala

---
+++ ch1.scala
@@ -0,0 +1,31 @@
+// ch1.2
+import math.sqrt
+var res = sqrt(3)
+println(res)
+println(res * res)
+println()
+
+// ch1.3
+// res is val 
+
+// ch1.4
+println("crazy " * 3)
+println()
+
+// ch1.5
+println(10 max 2)
+//println(10.max(2))
+println()
+
+// ch1.6
+println(BigInt(2) pow 1024)
+println()
+
+// ch1.8
+println(BigInt("427885438239754385432345666") toString 36) 
+println()
+
+// ch1.9
+println("first char of abcd:" + "abcd"(0))
+println("last char of abcd:" + "abcd"("abcd".length - 1)) 
+println()
