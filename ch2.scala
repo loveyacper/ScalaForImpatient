d53ea18cb1df3935e48b44403e2fded08115bf10
loveyacper <bertyoung666@gmail.com>
2015-07-05 11:54:02 +0800

    Update ch2.scala
    Create ch3.scala

--- ch2.scala
+++ ch2.scala
@@ -26,10 +26,47 @@ println()
 
 // 2.6
 def multiunicode(s : String) = {
-    var res = 1
+    var res : Long = 1
     for (c <- s)
-        res *= c
+        res *= c.toLong
     res
 }
 println(multiunicode("Hello"))
 println()
+
+// 2.7 
+println("Hello".foldLeft(1L)(_*_.toLong))
+
+// 2.9 
+def prod(str : String) : Long  = { 
+    if (str.length == 0)
+        0   
+    else if (str.length == 1)
+        str(0).toLong
+    else
+        str(0)*prod(str.substring(1))
+}
+println(prod("Hello"))
+
+// 2.10
+def power(x : Int, n : Int) : Float = { 
+    if (n == 0) {
+        1   
+    }   
+    else if (n < 0) {
+        1 / power(x, -n) 
+    }   
+    else {
+        if (n % 2 == 0)
+            power(x, n / 2) * power(x, n / 2)
+        else
+            x * power(x, n - 1)
+    }   
+}
+
+println(power(2, 0)) 
+println(power(2, -2))
+println(power(2, 2)) 
+println(power(2, -3))
+println(power(2, 3)) 
+
