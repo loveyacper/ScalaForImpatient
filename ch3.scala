e5628ce370e03266b322fd6703e86eee1c7d0176
loveyacper <bertyoung666@gmail.com>
2015-09-12 18:36:54 +0800

    Update ch3.scala

--- ch3.scala
+++ ch3.scala
@@ -21,3 +21,36 @@ def swapElem(arr : Array[Int]) = {
         }   
     }   
 }
+
+// 3.3
+def yieldSwapElem(a : Array[Int]) = {
+    for (i <- 0 until a.length) yield {
+        if (i % 2 == 0) {
+            if (i + 1 < a.length)
+                a(i + 1)
+            else
+                a(i)
+        }
+        else {
+            a(i - 1)
+        }
+    }
+}
+
+val arr1 = Array(1, 2, 3, 4, 5)
+val arr2 = yieldSwapElem(arr1)
+println(arr2.mkString("-"))
+
+// 3.4
+def  splitArray(a : Array[Int]) = {
+    a.filter(_ > 0) ++ a.filter(_ <= 0)
+}
+
+val arr3 = Array(1, -2, 3, 0, -5)
+val res = splitArray(arr3)
+println(res.mkString(","))
+
+// 3.5
+def  averageDoubleArr(a : Array[Double]) : Double = {
+    a.sum / a.size
+}
